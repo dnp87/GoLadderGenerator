fdcbdd7cb2d07706f8bac33cd1333d77ce625e9d
dnp87 <daniil.polyanskiy@gmail.com>
2016-04-02 20:08:18 +0300

    Getting canvas inner coords

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,8 @@ $(document).ready(function()
 	var cnv = $("#myCanvas")[0];
 	var ge = new $pr.GraphicsEngine(cnv, 600, 600, 30);
 	ge.drawBoard();
+	
+	$(cnv).click(function(e) {
+		alert(ge.getBoardCoords(e));
+	})
 });
