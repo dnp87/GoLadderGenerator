e162adf015c09d1a290eca08f916229bdcd50d2f
dnp87 <daniil.polyanskiy@gmail.com>
2016-04-03 17:31:13 +0300

    syntax fix

--- scripts/board.js
+++ scripts/board.js
@@ -6,16 +6,20 @@ if (!$pr) {
 
 $pr.BoardPosition = function() {
 	var that = this;
-	that.body = new Array[19];
+	that.body = new Array(19);
 	for(var i = 0; i < 19; i++ ) {
-		that.body[i] = new Array[19];
-		for (var i = 0; i < that.body[i].length; j++) {
+		that.body[i] = new Array(19);
+		for (var j = 0; j < that.body[i].length; j++) {
 			that.body[i][j] = '';
 		};
 	}
 }
 
-$pr.BoardPosition.placeStone = function(x, y, stoneType) {
+$pr.BoardPosition.prototype.getPosition = function(x, y) {
+	return this.body[x][y];
+}
+
+$pr.BoardPosition.prototype.placeStone = function(x, y, stoneType) {
 	var that = this;
 	if(stoneType != that.proto.BlackStone && stoneType != that.proto.WhiteStone) {
 		alert('invalid stone type');
