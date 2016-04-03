81367ccb101484ef2ba38ed3fca4e30cfccadb08
dnp87 <daniil.polyanskiy@gmail.com>
2016-04-03 17:23:31 +0300

    board position draft

---
+++ scripts/board.js
@@ -0,0 +1,32 @@
+//legend for board: '' - empty, 'b' - black, 'w' - white
+
+if (!$pr) {
+    var $pr = {};
+}
+
+$pr.BoardPosition = function() {
+	var that = this;
+	that.body = new Array[19];
+	for(var i = 0; i < 19; i++ ) {
+		that.body[i] = new Array[19];
+		for (var i = 0; i < that.body[i].length; j++) {
+			that.body[i][j] = '';
+		};
+	}
+}
+
+$pr.BoardPosition.placeStone = function(x, y, stoneType) {
+	var that = this;
+	if(stoneType != that.proto.BlackStone && stoneType != that.proto.WhiteStone) {
+		alert('invalid stone type');
+	}
+	else
+	{
+		//todo: add other function or place complete logic here
+		that.body[x][y] = stoneType;
+	}
+}
+
+$pr.BoardPosition.Empty = '';
+$pr.BoardPosition.BlackStone = 'B';
+$pr.BoardPosition.WhiteStone = 'W';
--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ $(document).ready(function()
 	var cnv = $("#boardCanvas")[0];
 	var ge = new $pr.GraphicsEngine(cnv, 600, 600, 30);
 	ge.drawBoard();
+	var board = new $pr.BoardPosition();
 	
 	$(cnv).click(function(e) {
 		var coords = ge.getBoardCoords(e);
